9d3e3d30af636394760c89bee770f75ee98ed421
swipe <zonder2009@gmail.com>
2023-10-01 18:08:33 +0300

    register page full

--- template/register.php
+++ template/register.php
@@ -1,30 +1,45 @@
-<?php
-// REGISTER PAGE TEMPLATE
+<!-- 
+    REGISTER PAGE TEMPLATE 
+-->
+<?php require_once 'template/header.php'; ?>
 
-if (isset($_POST['submit'])) {
-    $err = [];
-    if (strlen($_POST['login']) < 4 or strlen($_POST['login']) > 30) {
-        $err[] = "Логін повинен бути не меньше 4 символів і не більше 30";
-    }
-    if (isLoginExist($_POST['login'])) {
-        $err[] = "Такий логін існує";
-    }
-    if (count($err) === 0) {
-        createUser($_POST['login'], $_POST['password']);
-        header('Location: /login');
-        exit();
-    } else {
-        echo '<div>Помилка реестрації</div>';
-        foreach ($err as $error) {
-            echo $error . '<br>';
-        }
-    }
-}
-?>
+<main class="main main-flex">
+    <div class="register-section">
+        <h2>Регістрація</h2>
+        <form method="POST" class="register-form">
+            <div> Логін:</div>
+            <input class="reg-inpt" type="text" name="login" placeholder="Login" required>
+            <div>Пароль:</div>
+            <input class="reg-inpt" type="password" name="password" placeholder="Password" required>
+            <button class="reg-btn" type="submit" name="submit">
+                <span class="material-symbols-outlined">how_to_reg</span>Зарееструватися
+            </button>
+        </form>
+        <div class="error-section">
+            <?php
+            if (isset($_POST['submit'])) {
+                $err = [];
+                if (strlen($_POST['login']) < 4 or strlen($_POST['login']) > 30) {
+                    $err[] = "<span>Логін повинен бути не меньше 4 символів <br> і не більше 30!</span>";
+                }
+                if (isLoginExist($_POST['login'])) {
+                    $err[] = "<span>Такий логін вже існує</span>";
+                }
+                if (count($err) === 0) {
+                    createUser($_POST['login'], $_POST['password']);
+                    header('Location: /login');
+                    exit();
+                } else {
+                    echo '<div>*Помилка реестрації:</div>';
+                    foreach ($err as $error) {
+                        echo $error;
+                    }
+                }
+            }
+            ?>
+        </div>
+        <div class="message-if-reg">Вже маєте аккаунт? <a class="link-form-login" href="/login">Вхід</a></div>
+    </div>
+</main>
 
-<h2>Регістрація</h2>
-<form method="POST">
-    Login: <input type="text" name="login" required><br>
-    Password: <input type="text" name="password" required><br>
-    <input type="submit" name="submit" value="Зарееструватися">
-</form>
+<?php require_once 'template/footer.php' ?>
