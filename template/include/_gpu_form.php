898385ebc7ce2fc3638b07cc6f60bc9099031a2a
swipe <zonder2009@gmail.com>
2023-10-14 16:35:09 +0300

    add some css in article gpu-create + form

--- template/include/_gpu_form.php
+++ template/include/_gpu_form.php
@@ -5,18 +5,15 @@
       <p>URL: <input type="text" name="url" value="<?php if (isset($result['url'])) echo $result['url']; ?>"></p>
       <p>Part NUM: <input type="text" name="part-num" value="<?php if (isset($result['partNum'])) echo $result['partNum']; ?>"></p>
       <p>Vendor: <input type="text" name="vendor" value="<?php if (isset($result['vendor'])) echo $result['vendor']; ?>"></p>
-      <p>GPU Clock: <input type="number" name="gpu-clock" value="<?php if (isset($result['gpuClock'])) echo $result['gpuClock']; ?>"></p>
-      <p>Boost Clock: <input type="number" name="boost-clock" value="<?php if (isset($result['boostClock'])) echo $result['boostClock']; ?>"></p>
-      <p>Memory Clock: <input type="number" name="memory-clock" value="<?php if (isset($result['memoryClock'])) echo $result['memoryClock']; ?>"></p>
+      <p>GPU Clock (MHz):<br> <input type="number" name="gpu-clock" value="<?php if (isset($result['gpuClock'])) echo $result['gpuClock']; ?>"></p>
+      <p>Boost Clock (MHz):<br> <input type="number" name="boost-clock" value="<?php if (isset($result['boostClock'])) echo $result['boostClock']; ?>"></p>
+      <p>Memory Clock (MHz):<br> <input type="number" name="memory-clock" value="<?php if (isset($result['memoryClock'])) echo $result['memoryClock']; ?>"></p>
       <p>Other Changes: <input type="text" name="other-change" value="<?php if (isset($result['otherChange'])) echo $result['otherChange']; ?>"></p>
-
-
 
 
 
       <p>Description: <textarea name="description"><?php if (isset($result['description'])) echo $result['description']; ?></textarea>
       </p>
-
 
 
       <p>Category:
