e58c20c5a99ddeab03a6ea96220c1be3aa400243
swipe <zonder2009@gmail.com>
2023-11-11 23:52:52 +0200

    сторінка категорії + таблиця з характеристиками

--- template/gpudb.php
+++ template/gpudb.php
@@ -1,22 +1,50 @@
 <?php require_once 'template/header.php'; ?>
 
-<!-- GPUDB CATEGORY
-ВИВІД КАТЕГОРІЇ gpudb -->
+<!-- GPUDB CATEGORY -->
 
 <main class="main">
   <?php
   $out = '';
-  $out .= '<h1 class="title">' . $gpudb['title'] . '</h1>';
+  $out .= '<div class="wrap-category">';
+  $out .= '<div class="left-category-side">';
   $out .= '<img class="responsive-img gpudb-img" src="/static/images/gpucat/' . $gpudb['img'] . '">';
-  $out .= '<ul class="spec-list">';
-  $out .= '<li>' . $gpudb['graph-processor'] . '</li>';
-  $out .= '<li>' . $gpudb['cores'] . '</li>';
-  $out .= '<li>' . $gpudb['tmus'] . '</li>';
-  $out .= '<li>' . $gpudb['rops'] . '</li>';
-  $out .= '<li>' . $gpudb['memory-size'] . ' ' . 'gb' . '</li>';
-  $out .= '<li>' . $gpudb['memory-type'] . '</li>';
-  $out .= '<li>' . $gpudb['bus-width'] . ' ' . '<span>bit</span>' . '</li>';
-  $out .= '</ul>';
+  $out .= '</div>';
+  $out .= '<div class="rigth-category-side">';
+  $out .= '<h1 class="title"><span>Референс </span>' . $gpudb['title'] . '</h1>';
+  $out .= '<table class="category-item-info">';
+  $out .= '<tbody>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Графічний Чіп</td>';
+  $out .= '<td>' . $gpudb['graph-processor'] . '</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">К-сть Ядер</td>';
+  $out .= '<td>' . $gpudb['cores'] . '</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Tmus</td>';
+  $out .= '<td>' . $gpudb['tmus'] . '</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Rops</td>';
+  $out .= '<td>' . $gpudb['rops'] . '</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Обʼєм памʼяті</td>';
+  $out .= '<td>' . $gpudb['memory-size'] . ' GB</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Тип памʼяті</td>';
+  $out .= '<td>' . $gpudb['memory-type'] . '</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Ширина шини</td>';
+  $out .= '<td>' . $gpudb['bus-width'] . ' bit</td>';
+  $out .= '</tr>';
+  $out .= '</tbody>';
+  $out .= '</table>';
+  $out .= '</div>';
+  $out .= '</div>';
   $out .= '<div class="content-gpudb">' . $gpudb['description'] . '</div>';
   echo $out;
 
