8b7694e6568bb35697f5f5e08a5fc4ef32586f5c
swipe <zonder2009@gmail.com>
2023-11-11 21:33:51 +0200

    add відтворення останніх плат вендорів на головній сторінці  main.php

--- template/admin.php
+++ template/admin.php
@@ -1,4 +1,5 @@
 <?php
+// ob_start(); // !?
 if (!getUser()) {
     header("Location: /login");
 }
--- template/main.php
+++ template/main.php
@@ -7,12 +7,31 @@
   <h1 class="title">GPU DataBase</h1>
   <?php require_once 'template/include/_main-founders-table.php' ?>
   <div class="article-title">
-    <h1>Останні статті</h1>
+    <h2>Останні відеоадаптери від вендорів</h2>
+  </div>
+  <div class="article-row">
+    <?php
+    $queryTable = "SELECT vendor, url, title, img FROM gpuarticle";
+    $queryVendors = select($queryTable);
+
+    $vendorOut = '';
+    for ($i = 0; $i < 4; $i++) {
+      $vendorOut .= '<div class="article-item">';
+      $vendorOut .= '<span class="vendor-label vendor-title"><span class="material-symbols-outlined">star</span>' . $queryVendors[$i]['vendor'] . '</span>';
+      $vendorOut .= '<div><img class="img-article-title" src="/static/images/vendor/' . $queryVendors[$i]['img'] . '"></div>';
+      $vendorOut .= '<a class="article-item-link" href="/vendor/' . $queryVendors[$i]['url'] . '">' . $queryVendors[$i]['title'] . '</a>';
+      $vendorOut .= '</div>';
+    }
+    echo $vendorOut;
+    ?>
+  </div>
+  <div class="article-title">
+    <h2>Останні статті</h2>
   </div>
   <div class="article-row">
     <?php
     $out = '';
-    for ($i = 0; $i < count($result); $i++) {
+    for ($i = 0; $i < 4; $i++) {
       $out .= '<div class="article-item">';
       $out .= '<div><img class="img-article-title" src="/static/images/' . $result[$i]['image'] . '"></div>';
       $out .= '<a class="article-item-link" href="/article/' . $result[$i]['url'] . '">' . $result[$i]['title'] . '</a>';
