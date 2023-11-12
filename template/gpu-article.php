f5cecf641d6e40f4be10d0132f645cf0a632c054
swipe <zonder2009@gmail.com>
2023-11-12 15:32:27 +0200

    перероблена сторінка виведення вендора

--- template/gpu-article.php
+++ template/gpu-article.php
@@ -1,25 +1,99 @@
 <?php require_once 'template/header.php'; ?>
 
 <!-- VENDOR ARTICLE
-ВИВІД ПОВНОГО МАТЕРІАЛУ VENDOR -->
+ВИВЕДЕННЯ ПОВНОГО МАТЕРІАЛУ VENDOR -->
 
 <main class="main">
 
   <?php
   $out = '';
-  $out .= '<h1 class="title">' . $result['title'] . '</h1>';
-  $out .= '<div class="part-num">' . $result['part-num'] . '</div>';
+  $out .= '<div class="wrap-category">';
+
+  $out .= '<div class="left-category-side">';
   $out .= '<img class="responsive-img gpudb-img" src="/static/images/vendor/' . $result['img'] . '">';
-  $out .= '<ul class="spec-list">';
-  $out .= '<li>' . $result['graph-processor'] . '</li>';
-  $out .= '<li>' . $result['cores'] . '</li>';
-  $out .= '<li>' . $result['tmus'] . '</li>';
-  $out .= '<li>' . $result['rops'] . '</li>';
-  $out .= '<li>' . $result['memory-size'] . ' ' . 'gb' . '</li>';
-  $out .= '<li>' . $result['memory-type'] . '</li>';
-  $out .= '<li>' . $result['bus-width'] . ' ' . '<span>bit</span>' . '</li>';
-  $out .= '</ul>';
-  $out .= '<div>' . $result['description'] . '</div>';
+  $out .= '</div>';
+
+  $out .= '<div class="rigth-category-side bg-gray b-radius-1">';
+  $out .= '<h1 class="title"><span>Retail</span> ' . $result['title'] . '</h1>';
+  $out .= '<div class="part-num"><span>Part Number: </span>' . $result['part-num'] . '</div>';
+
+  $out .= '<table class="category-item-info">';
+
+  $out .= '<tbody>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Vendor</td>';
+  $out .= '<td><span class="vendor-btn">' . $result['vendor'] . '</span></td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Графічний Чіп</td>';
+  $out .= '<td>' . $result['graph-processor'] . '</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">К-сть Ядер</td>';
+  $out .= '<td>' . $result['cores'] . '</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Tmus</td>';
+  $out .= '<td>' . $result['tmus'] . '</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Rops</td>';
+  $out .= '<td>' . $result['rops'] . '</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Обʼєм памʼяті</td>';
+  $out .= '<td>' . $result['memory-size'] . ' GB</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Тип памʼяті</td>';
+  $out .= '<td>' . $result['memory-type'] . '</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Ширина шини</td>';
+  $out .= '<td>' . $result['bus-width'] . ' bit</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">GPU Clock</td>';
+  $out .= '<td>' . $result['gpu-clock'] . ' Mhz</td>';
+  $out .= '</tr>';
+  $out .= '</tbody>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Boost Clock</td>';
+  $out .= '<td>' . $result['boost-clock'] . ' Mhz</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Memory Clock</td>';
+  $out .= '<td>' . $result['memory-clock'] . ' Mhz</td>';
+  $out .= '</tr>';
+  $out .= '<tr>';
+  $out .= '<td class="bold">Особливості</td>';
+  $out .= '<td>' . $result['other-change'] . '</td>';
+  $out .= '</tr>';
+  $out .= '</tbody>';
+
+  $out .= '</table>';
+
+  $out .= '</div>';
+
+  $out .= '</div>';
+  $out .= '<div class="content-gpudb">' . $result['description'] . '</div>';
+
+
+
+
+  // $out .= '<h1 class="title"><span>Retail:</span> ' . $result['title'] . '</h1>';
+  // $out .= '<div class="part-num">' . $result['part-num'] . '</div>';
+  // $out .= '<img class="responsive-img gpudb-img" src="/static/images/vendor/' . $result['img'] . '">';
+  // $out .= '<ul class="spec-list">';
+  // $out .= '<li>' . $result['graph-processor'] . '</li>';
+  // $out .= '<li>' . $result['cores'] . '</li>';
+  // $out .= '<li>' . $result['tmus'] . '</li>';
+  // $out .= '<li>' . $result['rops'] . '</li>';
+  // $out .= '<li>' . $result['memory-size'] . ' ' . 'gb' . '</li>';
+  // $out .= '<li>' . $result['memory-type'] . '</li>';
+  // $out .= '<li>' . $result['bus-width'] . ' ' . '<span>bit</span>' . '</li>';
+  // $out .= '</ul>';
+  // $out .= '<div>' . $result['description'] . '</div>';
 
   echo $out;
   ?>
